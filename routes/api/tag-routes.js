04511c4a478ab01dd697ff2e1a8f15e4e2e20e8a
palminski <williambolls98@gmail.com>
2022-09-30 09:26:11 -0700

    get routes in place!

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,16 +1,56 @@
 const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const { Tag, Product, ProductTag, Category } = require('../../models');
 
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include:
+      {
+        model: Product,
+        attributes: ['product_name','price','stock'],
+        
+        include: {
+          model: Category,
+          attributes: ['category_name']
+        }
+      }
+    
+  })
+  .then(dataResponse => res.json(dataResponse))
+  .catch( err => {
+    console.log("An error has occured");
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    include:[
+      {
+        model: Product,
+        attributes: ['product_name','price','stock'],
+        include: {
+          model: Category,
+          attributes: ['category_name']
+        }
+      }
+    ],
+    where:{
+      id: req.params.id
+    }
+  })
+  .then(dataResponse => res.json(dataResponse))
+  .catch( err => {
+    console.log("An error has occured");
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
