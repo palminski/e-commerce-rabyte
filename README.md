aad33240d976ed206cee140c0f430b4275104160
palminski <williambolls98@gmail.com>
2022-09-30 10:58:58 -0700

    added all routes and a readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# E-Commerce-Back-End
+
+## Description
+Serves api requests to update a database containing product data.
+A video of the application in action can be seen [here](https://drive.google.com/file/d/16NIXf7P5SQeX3jlsmRLzE9UAlU0lSqwM/view).
+
+## Table of Contents
+
+[Usage](#usage)
+[Questions](#questions)
+
+## Usage
+Make sure that your SQL log in information is set up in a .env file and all dependencies are installed. 
+Start by seeding the server using the following command in the mysql shell: source db/schema.sql
+Next, from root direcotry run the command npm run seed. 
+This will set up the database. 
+Once the DB is set up run the npm start command and the erver will begin running and you can make API requests to it
+.env variables are as follows: 
+DB_NAME = "ecommerce_db"
+DB_USER ="yourUSerName"
+DB_PW="yourPassword"
+
+## Questions?
+contact me!
+my Github page can be viewed [here](https://github.com/palminski)
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -55,14 +55,55 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+  .then(dataResponse => res.json(dataResponse))
+  .catch(err => {
+    console.log('An error has occured');
+    console.log(err)
+    res.status(500).json(err);
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dataResponse => {
+    if (dataResponse[0] === 0) {
+      res.status(404).json({message: "An error has occured"});
+      return;
+    }
+    res.json(dataResponse);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dataResponse => {
+    if (!dataResponse) {
+      res.status(404).json({ message: "Item not found"})
+      return;
+    }
+    res.json(dataResponse);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
